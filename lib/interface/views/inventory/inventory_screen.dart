f5b37efbb7cbd6c1f5461d1abc9803050ee2a7be
sebaszap123-dev <sebaszap123@gmail.com>
2024-02-23 21:49:54 -0600

    carousel

--- lib/interface/views/inventory/inventory_screen.dart
+++ lib/interface/views/inventory/inventory_screen.dart
@@ -1,8 +1,9 @@
-import 'package:anju/interface/widgets/anju_item_list_viewer.dart';
-import 'package:anju/interface/widgets/inventory/categories_widgets.dart';
-import 'package:anju/interface/widgets/inventory/product_item.dart';
+import 'package:anju/config/utils/utils.dart';
+import 'package:anju/interface/widgets/anju_carousel.dart';
+// import 'package:anju/interface/widgets/anju_item_list_viewer.dart';
+// import 'package:anju/interface/widgets/inventory/categories_widgets.dart';
+// import 'package:anju/interface/widgets/inventory/product_item.dart';
 import 'package:auto_route/annotations.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
@@ -13,13 +14,27 @@ class InventoryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AnjuItemListViewer(
-      childBuilder: (p0) => const CategoryWidget(
-        icon: Icons.category,
-        text: 'Materiales',
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+        child: Column(
+          children: [
+            AnjuCarousel(
+                images: List.generate(
+              2,
+              (index) => AnjuImages.test,
+            )),
+          ],
+        ),
       ),
-      list: List.generate(10, (index) => null),
-      title: 'Inventario',
     );
+    // return AnjuItemListViewer(
+    //   childBuilder: (p0) => const CategoryWidget(
+    //     icon: Icons.category,
+    //     text: 'Materiales',
+    //   ),
+    //   list: List.generate(10, (index) => null),
+    //   title: 'Inventario',
+    // );
   }
 }
--- lib/interface/views/show/amugurumi_details.dart
+++ lib/interface/views/show/amugurumi_details.dart
@@ -8,7 +8,7 @@ class AmigurumiDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       appBar: AnjuTopBar(),
       body: SingleChildScrollView(),
     );
