d444beed8436285494bdb041a9d1bf85c277dd96
sebaszap123-dev <sebaszap123@gmail.com>
2024-02-29 13:10:58 -0600

    add label

--- lib/interface/widgets/forms/anju_textfield.dart
+++ lib/interface/widgets/forms/anju_textfield.dart
@@ -2,13 +2,16 @@ import 'package:anju/config/themes/anju_colors.dart';
 import 'package:flutter/material.dart';
 
 class AnjuTextField extends StatelessWidget {
-  const AnjuTextField({super.key, required this.controller});
+  const AnjuTextField(
+      {super.key, required this.controller, required this.label});
   final TextEditingController controller;
+  final String label;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: controller,
       decoration: InputDecoration(
+        labelText: 'hola',
         focusedBorder: _myBorders(),
         enabledBorder: _myBorders(color: AnjuColors.primaryShade100),
         errorBorder: _myBorders(color: AnjuColors.secondary),
